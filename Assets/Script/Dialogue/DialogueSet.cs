d8c8ea2d91cc180c2f91a8d8abbdd75a155088e8
Crocobob911 <mjdaniel911@naver.com>
2021-03-06 00:19:56 +0900

    커밋 3
    2021.03.06
    대화창 출력 기본 매커니즘 구현

--- Assets/Script/Dialogue/DialogueSet.cs
+++ Assets/Script/Dialogue/DialogueSet.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class DialogueSet : MonoBehaviour // 저장된 TextData에서 데이터를 들고와 
-                                                                    // 출력할 준비하는 역할
+public class DialogueSet : MonoBehaviour // 텍스트 파일들의 경로를 textData에 저장하고
+                                         // 저장된 TextData에서 데이터를 들고와 
+                                         // 출력할 준비하는 역할
 {
-    public DialogueTextData textData;
+    public DialoguePathManager textData;
 
     private void Start()
     {
-        textData = GameObject.Find("DialogueTextData").GetComponent<DialogueTextData>();
+        textData = GameObject.Find("DialoguePathManager").GetComponent<DialoguePathManager>();
     }
 
     public void SetText(int index)
     {
         var system = GameObject.Find("DialogueSystem").GetComponent<DialogueSystem>();
 
-        var data = JsonUtility.FromJson<Dialogue>(File.ReadAllText(textData.jsonPath[index]));
+        var data = JsonUtility.FromJson<Dialogue>(File.ReadAllText(textData.DialaoguePath[index]));
         system.DialogueBegin(data);
     }
 }
